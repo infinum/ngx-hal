c2d1fed4204b432660b5da7e3f924718f7dc7242
Mihael Safaric <mihael.safaric@gmail.com>
2019-03-01 15:20:10 +0100

    Move fetching models to datastore

--- projects/ngx-hal/src/lib/services/datastore/datastore.service.ts
+++ projects/ngx-hal/src/lib/services/datastore/datastore.service.ts
@@ -1,15 +1,24 @@
 import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { NetworkConfig } from '../../interfaces/network-config.interface';
 import { HalModel } from '../../models/hal.model';
 import { HalDocument } from '../../classes/hal-document';
 import { ModelConstructor } from '../../types/model-constructor.type';
 import { HAL_DOCUMENT_CLASS_METADATA_KEY } from '../../constants/metadata.constant';
 import { HalDocumentConstructor } from '../../types/hal-document-construtor.type';
+import { RequestOptions } from '../../types/request-options.type';
+import { DEFAULT_REQUEST_OPTIONS } from '../../constants/request.constant';
+import { RawHalResource } from '../../interfaces/raw-hal-resource.interface';
 
 export class DatastoreService {
   public networkConfig: NetworkConfig;
 
   constructor(public http: HttpClient) {}
+
+  private getHalDocumentClass<T extends HalModel>(): HalDocumentConstructor<T> {
+    return Reflect.getMetadata(HAL_DOCUMENT_CLASS_METADATA_KEY, this.constructor) || HalDocument;
+  }
 
   public buildUrl(model?: HalModel): string {
     const urlParts: Array<string> = [
@@ -26,7 +35,63 @@ export class DatastoreService {
     return new halDocumentClass(response, modelClass);
   }
 
-  private getHalDocumentClass<T extends HalModel>(): HalDocumentConstructor<T> {
-    return Reflect.getMetadata(HAL_DOCUMENT_CLASS_METADATA_KEY, this.constructor) || HalDocument;
+  public findOne<T extends HalModel>(
+    modelClass: ModelConstructor<T>,
+    modelId: string,
+    requestOptions: RequestOptions = {}
+  ): Observable<T> {
+    const url: string = this.buildModelUrl(modelClass, modelId);
+
+    const options = Object.assign(DEFAULT_REQUEST_OPTIONS, requestOptions);
+
+    return this.http.get<T>(url, options).pipe(
+      map((response: HttpResponse<T>) => {
+        return new modelClass(this.extractResourceFromResponse(response), response);
+      })
+    );
+  }
+
+  public find<T extends HalModel>(modelClass: ModelConstructor<T>, params: object): Observable<Array<T>>;
+  public find<T extends HalModel>(modelClass: ModelConstructor<T>, params: object, includeMeta: boolean): Observable<Array<T>>;
+  public find<T extends HalModel>(
+    modelClass: ModelConstructor<T>,
+    params: object, includeMeta: boolean,
+    requestOptions: RequestOptions
+  ): Observable<HalDocument<T>>;
+  public find<T extends HalModel>(
+    modelClass: ModelConstructor<T>,
+    params: object = {},
+    includeMeta: boolean = false,
+    requestOptions: RequestOptions = {}
+  ): Observable<HalDocument<T>> | Observable<Array<T>> {
+    const url: string = this.buildModelUrl(modelClass);
+
+    const options = Object.assign({}, DEFAULT_REQUEST_OPTIONS, requestOptions);
+    Object.assign(options.params, params);
+
+    if (includeMeta) {
+      return this.http.get<T>(url, options).pipe(
+        map((response: HttpResponse<T>) => {
+          const halDocument: HalDocument<T> = this.createHalDocument(response, modelClass);
+          return halDocument;
+        })
+      );
+    }
+
+    return this.http.get<T>(url, options).pipe(
+      map((response: HttpResponse<T>) => {
+        const halDocument: HalDocument<T> = this.createHalDocument(response, modelClass);
+        return halDocument.models;
+      })
+    );
+  }
+
+  private buildModelUrl(modelClass: ModelConstructor<HalModel>, modelId?: string): string {
+    const modelUrl: string = this.buildUrl(new modelClass());
+    return modelId ? `${modelUrl}/${modelId}` : modelUrl;
+  }
+
+  private extractResourceFromResponse(response: HttpResponse<object>): RawHalResource {
+    return response.body;
   }
 }
--- projects/ngx-hal/src/lib/services/model-service/model.service.ts
+++ projects/ngx-hal/src/lib/services/model-service/model.service.ts
@@ -1,29 +1,16 @@
 import { HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 import { HalModel } from '../../models/hal.model';
 import { DatastoreService } from '../datastore/datastore.service';
-import { RawHalResource } from '../../interfaces/raw-hal-resource.interface';
 import { RequestOptions } from '../../types/request-options.type';
-import { DEFAULT_REQUEST_OPTIONS } from '../../constants/request.constant';
 import { HalDocument } from '../../classes/hal-document';
 import { ModelConstructor } from '../../types/model-constructor.type';
-import { HAL_DOCUMENT_CLASS_METADATA_KEY } from '../../constants/metadata.constant';
-import { HalDocumentConstructor } from '../../types/hal-document-construtor.type';
 
 export abstract class ModelService<Model extends HalModel> {
   constructor(protected datastore: DatastoreService, private modelClass: ModelConstructor<Model>) {}
 
   public findOne(modelId: string, requestOptions: RequestOptions = {}): Observable<Model> {
-    const url: string = this.buildModelUrl(modelId);
-
-    const options = Object.assign(DEFAULT_REQUEST_OPTIONS, requestOptions);
-
-    return this.datastore.http.get<Model>(url, options).pipe(
-      map((response: HttpResponse<Model>) => {
-        return this.createModel(this.extractResourceFromResponse(response), response);
-      })
-    );
+    return this.datastore.findOne<Model>(this.modelClass, modelId, requestOptions);
   }
 
   public find(params: object): Observable<Array<Model>>;
@@ -34,26 +21,7 @@ export abstract class ModelService<Model extends HalModel> {
     includeMeta: boolean = false,
     requestOptions: RequestOptions = {}
   ): Observable<HalDocument<Model>> | Observable<Array<Model>> {
-    const url: string = this.buildModelUrl();
-
-    const options = Object.assign({}, DEFAULT_REQUEST_OPTIONS, requestOptions);
-    Object.assign(options.params, params);
-
-    if (includeMeta) {
-      return this.datastore.http.get<Model>(url, options).pipe(
-        map((response: HttpResponse<Model>) => {
-          const halDocument: HalDocument<Model> = this.createHalDocument(response);
-          return halDocument;
-        })
-      );
-    }
-
-    return this.datastore.http.get<Model>(url, options).pipe(
-      map((response: HttpResponse<Model>) => {
-        const halDocument: HalDocument<Model> = this.createHalDocument(response);
-        return halDocument.models;
-      })
-    );
+    return this.datastore.find(this.modelClass, params, includeMeta, requestOptions);
   }
 
   public createNewModel(recordData: object = {}): Model {
@@ -62,32 +30,5 @@ export abstract class ModelService<Model extends HalModel> {
 
   private createModel(recordData: object = {}, response?: HttpResponse<object>): Model {
     return new this.modelClass(recordData, response);
-  }
-
-  private createHalDocument(response: HttpResponse<Model>): HalDocument<Model> {
-    const halDocumentClass = this.getHalDocumentClass<Model>();
-
-    if (halDocumentClass) {
-      return new halDocumentClass(response, this.modelClass);
-    }
-
-    return this.datastore.createHalDocument<Model>(response, this.modelClass);
-  }
-
-  private buildModelUrl(modelId?: string): string {
-    const modelUrl: string = this.datastore.buildUrl(this.representableModel);
-    return modelId ? `${modelUrl}/${modelId}` : modelUrl;
-  }
-
-  private get representableModel(): Model {
-    return new this.modelClass();
-  }
-
-  private extractResourceFromResponse(response: HttpResponse<object>): RawHalResource {
-    return response.body;
-  }
-
-  private getHalDocumentClass<T extends HalModel>(): HalDocumentConstructor<T> {
-    return Reflect.getMetadata(HAL_DOCUMENT_CLASS_METADATA_KEY, this.constructor);
   }
 }
