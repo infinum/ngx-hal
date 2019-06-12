707289255141861ca52a375bd48380edf3654c37
Mihael Safaric <mihael.safaric@gmail.com>
2019-06-12 19:26:08 +0200

    Fix extracting response headers

--- projects/ngx-hal/src/lib/utils/get-response-headers/get-response-header.util.ts
+++ projects/ngx-hal/src/lib/utils/get-response-headers/get-response-header.util.ts
@@ -1,6 +1,7 @@
-import { HttpResponse } from '@angular/common/http';
+import { HttpResponse, HttpHeaders } from '@angular/common/http';
 
 export function getResponseHeader(response: HttpResponse<any>, headerName: string): any {
-  const headers: object = response ? (response.headers || {}) : {};
-  return headers[headerName];
+  const emptyHeaders: HttpHeaders = new HttpHeaders();
+  const headers: HttpHeaders = response ? (response.headers || emptyHeaders) : emptyHeaders;
+  return headers.get(headerName);
 }
