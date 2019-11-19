8de3b9f9e3ac664e496b5cf8114c019025b3e4f9
Mihael Safaric <mihael.safaric@gmail.com>
2019-11-19 17:21:42 +0100

    Remove query param encoding from the lib

---
+++ projects/ngx-hal/src/lib/helpers/make-http-params/make-http-params.helper.ts
@@ -0,0 +1,11 @@
+import { HttpParams } from '@angular/common/http';
+
+export function makeHttpParams(params: object, httpParamsOptions?: object): HttpParams {
+  let httpParams: HttpParams = new HttpParams(httpParamsOptions);
+
+  Object.keys(params).forEach((paramKey: string) => {
+    httpParams = httpParams.append(paramKey, params[paramKey]);
+  });
+
+  return httpParams;
+}
--- projects/ngx-hal/src/lib/helpers/make-query-params-string/make-query-params-string.helper.ts
+++ projects/ngx-hal/src/lib/helpers/make-query-params-string/make-query-params-string.helper.ts
@@ -1,15 +1,12 @@
-import { encodeQueryParams } from '../../utils/encode-query-params/encode-query-params.utils';
-
 export function makeQueryParamsString(params: object, sortAlphabetically: boolean = false): string {
-  const encodedParams: object = encodeQueryParams(params);
-  let paramKeys: Array<string> = Object.keys(encodedParams);
+  let paramKeys: Array<string> = Object.keys(params);
 
   if (sortAlphabetically) {
     paramKeys = paramKeys.sort();
   }
 
   const queryParamsString: string = paramKeys.reduce((paramsString: string, queryParamKey: string) => {
-    return `${paramsString}&${queryParamKey}=${encodedParams[queryParamKey]}`;
+    return `${paramsString}&${queryParamKey}=${params[queryParamKey]}`;
   }, '');
 
   return queryParamsString.slice(1);
