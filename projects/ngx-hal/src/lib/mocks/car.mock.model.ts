b79b57a3c3ee2a7993570ffba08762416c4cc00f
Mihael Šafarić <mihael.safaric@gmail.com>
2020-03-03 23:47:28 +0100

    Refactor payload generation (#59)
    * Refactor payload generation

---
+++ projects/ngx-hal/src/lib/mocks/car.mock.model.ts
@@ -0,0 +1,30 @@
+import { HalModel } from '../models/hal.model';
+import { ModelConfig } from '../decorators/model-config.decorator';
+import { Attribute } from '../decorators/attribute.decorator';
+import { MockModel2 } from './mock-model-2';
+import { HasOne } from '../decorators/has-one.decorator';
+import { HasMany } from '../decorators/has-many.decorator';
+
+@ModelConfig({
+  type: 'Car',
+  endpoint: 'car'
+})
+export class CarModel extends HalModel {
+  @Attribute({
+    externalName: 'name'
+  })
+  carName: string;
+
+  @HasOne({
+    includeInPayload: true,
+    externalName: 'parentCompany'
+  })
+  company: MockModel2;
+
+  @HasMany({
+    itemsType: MockModel2,
+    includeInPayload: true,
+    externalName: 'parts'
+  })
+  carParts: Array<MockModel2>;
+}
