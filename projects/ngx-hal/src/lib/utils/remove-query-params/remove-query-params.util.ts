7a0da2a8c5013329a03af9bef371e8c12b39adbb
Mihael Šafarić <mihael.safaric@gmail.com>
2021-10-01 16:30:40 +0200

    Fix removing templated query parameters from uris (#101)

--- projects/ngx-hal/src/lib/utils/remove-query-params/remove-query-params.util.ts
+++ projects/ngx-hal/src/lib/utils/remove-query-params/remove-query-params.util.ts
@@ -1,9 +1,13 @@
 export function removeQueryParams(uri: string): string {
-  const splittedUri: Array<string> = uri.split('?');
+  const splittedUri: Array<string> = hasOnlyTemplatedQueryParameters(uri) ? uri.split('{?') : uri.split('?');
 
   if (splittedUri.length > 1) {
     splittedUri.pop();
   }
 
   return splittedUri.join('');
+}
+
+function hasOnlyTemplatedQueryParameters(uri: string): boolean {
+  return uri.indexOf('{?') !== -1;
 }
