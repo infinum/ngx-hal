555315253ab8330a46acb5a7d0499d0e77427f46
Mihael Safaric <mihael.safaric@gmail.com>
2022-08-10 17:13:42 +0200

    Adapt specs for isSimpleHalInstance

--- projects/ngx-hal/src/lib/helpers/is-simple-hal-model-instance.ts/is-simple-hal-model-instance.helper.spec.ts
+++ projects/ngx-hal/src/lib/helpers/is-simple-hal-model-instance.ts/is-simple-hal-model-instance.helper.spec.ts
@@ -1,38 +1,45 @@
 import { TestBed } from '@angular/core/testing';
 import { HalModel } from '../../models/hal.model';
 import { DatastoreService } from '../../services/datastore/datastore.service';
-import { isHalModelInstance } from './is-hal-model-instance.helper';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { SimpleHalModel } from '../../models/simple-hal.model';
+import { isSimpleHalModelInstance } from './is-simple-hal-model-instance.helper';
 
 class InheritsHal extends HalModel {}
+class InheritsSimpleHal extends SimpleHalModel {}
 class DoesNotInherit {}
 
-describe('isHalModelInstance', () => {
+describe('isSimpleHalModelInstance', () => {
   beforeEach(() => TestBed.configureTestingModule({
     imports: [HttpClientTestingModule],
     providers: [DatastoreService]
   }));
 
-  it('should return true for an instance of a class which extends HalModel', () => {
-    const datastoreService: DatastoreService = TestBed.inject(DatastoreService);
-    const instance: InheritsHal = new InheritsHal({}, datastoreService);
-    expect(isHalModelInstance(instance)).toBe(true);
+  it('should return true for an instance of a class which extends SimpleHalModel', () => {
+    const instance: InheritsSimpleHal = new InheritsSimpleHal();
+    expect(isSimpleHalModelInstance(instance)).toBe(true);
   });
 
-  it('should return false for an instance of a class which does not extend HalModel', () => {
+  it('should return false for an instance of a class which extends HalModel', () => {
+    const datastoreService: DatastoreService = TestBed.inject(DatastoreService);
+    const instance: InheritsHal = new InheritsHal({}, datastoreService);
+    expect(isSimpleHalModelInstance(instance)).toBe(false);
+  });
+
+  it('should return false for an instance of a class which does not extend SimpleHalModel', () => {
     const instance: DoesNotInherit = new DoesNotInherit();
-    expect(isHalModelInstance(instance)).toBe(false);
+    expect(isSimpleHalModelInstance(instance)).toBe(false);
   });
 
   it('should return false for a string', () => {
-    expect(isHalModelInstance('notHal')).toBe(false);
+    expect(isSimpleHalModelInstance('notHal')).toBe(false);
   });
 
   it('should return false for an object', () => {
-    expect(isHalModelInstance({})).toBe(false);
+    expect(isSimpleHalModelInstance({})).toBe(false);
   });
 
   it('should return false for a number', () => {
-    expect(isHalModelInstance(1)).toBe(false);
+    expect(isSimpleHalModelInstance(1)).toBe(false);
   });
 });
