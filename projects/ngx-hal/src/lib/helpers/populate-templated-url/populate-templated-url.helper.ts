d51b18e81d689e5fbdd580aaf6296ff248456522
Mihael Safaric <mihael.safaric@gmail.com>
2023-01-26 13:28:25 +0100

    Handle passing falsy value to the UriTemplates lib

--- projects/ngx-hal/src/lib/helpers/populate-templated-url/populate-templated-url.helper.ts
+++ projects/ngx-hal/src/lib/helpers/populate-templated-url/populate-templated-url.helper.ts
@@ -3,5 +3,6 @@ import * as UriTemplates from 'uri-templates';
 const UriTemplate = UriTemplates.default || UriTemplates;
 
 export function populateTemplatedUrl(url: string, params?: object): string {
-	return new UriTemplate(url).fill(params);
+	const safeUrl = url || '';
+	return new UriTemplate(safeUrl).fill(params);
 }
