1fdce2fa97c2199d21547ccd4c2d8391e92441d1
Mihael Safaric <mihael.safaric@gmail.com>
2023-04-24 08:10:54 +0200

    Cleanup

--- projects/ngx-hal/src/lib/helpers/metadata/metadata.helper.ts
+++ projects/ngx-hal/src/lib/helpers/metadata/metadata.helper.ts
@@ -1,17 +1,3 @@
-import { ModelProperty } from '../../interfaces/model-property.interface';
-
-// export function getMetadata<T extends ModelProperty>(obj: Record<string, any>, metaKey: string): Array<T> {
-//   if (!Object.prototype.hasOwnProperty.call(obj, metaKey)) {
-//     Object.defineProperty(obj, metaKey, {
-//       configurable: false,
-//       enumerable: false,
-//       value: []
-//     });
-//   }
-
-//   return obj[metaKey];
-// }
-
 export function getObjProperty(obj, propertyKey: string, defaultValue: any = []) {
 	const objClass = getClass(obj);
 
@@ -31,7 +17,7 @@ export function setObjProperty(objClass, propertyKey, value) {
 	});
 }
 
-// returns a new array, therefore, pushing into it won't affect the class metadata
+// Returns a new array, therefore, pushing into it won't affect the class metadata
 export function getArrayObjProperty<T>(obj, propertyKey: string): Array<T> {
 	const objClass = getClass(obj);
 	const parentClass = Object.getPrototypeOf(objClass);
