d4f1b22c1a751239b6f89bcdfe7e6372a135ceee
Dani Pavic <35301355+danipavic@users.noreply.github.com>
2026-01-21 14:15:57 +0100

    Update projects/ngx-hal/src/lib/types/model-constructor.type.ts
    Co-authored-by: Filip Voska <filip.voska@gmail.com>

--- projects/ngx-hal/src/lib/types/model-constructor.type.ts
+++ projects/ngx-hal/src/lib/types/model-constructor.type.ts
@@ -2,7 +2,7 @@ import { Pagination } from '../classes/pagination';
 import { HalModel } from '../models/hal.model';
 
 export type ModelConstructor<T extends HalModel<P>, P extends Pagination> = {
-	new (...args: Array<any>): T;
+	new (...args: Array<unknown>): T;
 };
 export type ModelConstructorFn<T extends HalModel<P>, P extends Pagination> = (
 	rawPropertyValue: any,
